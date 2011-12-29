d63d5ee544a22b128d633aade6a21a20f195026a
Roman Verchykov <roman.verchikov@gmail.com>
2011-12-29 14:59:43 +0200

    Implemented function which calculates direction of a segment of points

--- ch03/ex09-direction_data_type.hs
+++ ch03/ex09-direction_data_type.hs
@@ -16,7 +16,8 @@ data Segment = LineSegment {
                    pt1 :: Point,
                    pt2 :: Point,
                    pt3 :: Point
-               } deriving (Show)
+               } 
+               deriving (Show)
 
 data Direction = TurnsLeft 
                | TurnsRight
@@ -26,18 +27,23 @@ data Direction = TurnsLeft
 -- Write a function that calculates the turn made by three 2D points and
 -- returns a Direction.
 --
-getSegmentDirection :: Segment -> Direction
-getSegmentDirection s 
+segmentDirection :: Segment -> Direction
+segmentDirection s 
     | lineEquation (pt1 s) (pt2 s) (pt3 s) > 0 = TurnsLeft
     | lineEquation (pt1 s) (pt2 s) (pt3 s) < 0 = TurnsRight
     | otherwise                                = StraightLine
         where lineEquation p1 p2 p3 = ((y p3) - (y p1)) * ((x p2) - (x p1)) - ( (y p2) - (y p1) ) * ( (x p3) - (x p1) )
 --                                    (  y    -   y1  )   (  x2   -   x1  ) - (    y2  -   y1   )   (   x    -   x1   )
 
-
-
 --------------------------------------------------------------------------------
 -- Define a function that takes a list of 2D points and computes the direction
 -- of each successive triple. Given a list of points [a,b,c,d,e], it should
 -- begin by computing the turn made by [a,b,c], then the turn made by [b,c,d],
 -- then [c,d,e]. Your function should return a list of Direction.
+
+segmentsDirections :: [Point] -> [Direction]
+segmentsDirections pts
+    | length pts > 2 = ( segmentDirectionFromPoints (pts) : (segmentsDirections (tail pts)) )
+    | otherwise      = []
+        where segmentDirectionFromPoints pts = segmentDirection (LineSegment (head pts) (head (tail pts)) (head (tail (tail pts))))
+
