dcc6657ba12705102da84478779f595376543439
Roman Verchykov <roman.verchikov@gmail.com>
2011-12-29 14:17:49 +0200

    Implemented function which calculates 3-line segment direction; based on equation of a line

--- ch03/ex09-direction_data_type.hs
+++ ch03/ex09-direction_data_type.hs
@@ -5,8 +5,8 @@
 --
 
 data Point = Point {
-                 xOf :: Double,
-                 yOf :: Double
+                 x :: Double,
+                 y :: Double
              } deriving (Show)
 
 data Line = Line Point Point
@@ -22,13 +22,22 @@ data Direction = TurnsLeft
                | TurnsRight
                | StraightLine 
                  deriving (Show)
-
-
+--------------------------------------------------------------------------------
+-- Write a function that calculates the turn made by three 2D points and
+-- returns a Direction.
+--
 getSegmentDirection :: Segment -> Direction
 getSegmentDirection s 
-    | angleBetween (line1 s) (line2 s) > 0 = TurnsLeft
-    | angleBetween (line1 s) (line2 s) < 0 = TurnsRight
-    | otherwise                            = StraightLine
-        where line1 s = (Line (pt1 s) (pt2 s))
-              line2 s = (Line (pt2 s) (pt3 s))
-              angleBetween l1 l2 = 
+    | lineEquation (pt1 s) (pt2 s) (pt3 s) > 0 = TurnsLeft
+    | lineEquation (pt1 s) (pt2 s) (pt3 s) < 0 = TurnsRight
+    | otherwise                                = StraightLine
+        where lineEquation p1 p2 p3 = ((y p3) - (y p1)) * ((x p2) - (x p1)) - ( (y p2) - (y p1) ) * ( (x p3) - (x p1) )
+--                                    (  y    -   y1  )   (  x2   -   x1  ) - (    y2  -   y1   )   (   x    -   x1   )
+
+
+
+--------------------------------------------------------------------------------
+-- Define a function that takes a list of 2D points and computes the direction
+-- of each successive triple. Given a list of points [a,b,c,d,e], it should
+-- begin by computing the turn made by [a,b,c], then the turn made by [b,c,d],
+-- then [c,d,e]. Your function should return a list of Direction.
