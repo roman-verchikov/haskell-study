411f2c05cf9b54f3f3ad5a16f024dc923f7d96d6
Roman Verchykov <roman.verchikov@gmail.com>
2012-01-20 08:14:19 +0200

    Slightly updated equality operation

--- ch06/eqclasses.hs
+++ ch06/eqclasses.hs
@@ -9,5 +9,12 @@ instance BasicEq Bool where
 class BasicEq2 a where
     isEqual2, isNotEqual2 :: a -> a -> Bool
 
-    isEqual2 x y = not (isNotEqual2 x y)
+    isEqual2    x y = not (isNotEqual2 x y)
     isNotEqual2 x y = not (isEqual2 x y)
+
+instance BasicEq2 Color where 
+    isEqual2 Red   Red   = True
+    isEqual2 Green Green = True
+    isEqual2 Blue  Blue  = True
+    isEqual2 _     _     = False
+    
